95218b4e62715ae83b1226b8e0204d1f563d0bba
sehee <s921132820@gmail.com>
2026-02-10 08:16:40 +0900

    국가 목록 select로 전환

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-(function () {
-  'use strict';
+// (function () {
+//   'use strict';
 
-  var app = document.getElementById('app');
-  if (!app) return;
+//   var app = document.getElementById('app');
+//   if (!app) return;
 
-  app.innerHTML = '<p class="loading">로딩 중...</p>';
+//   app.innerHTML = '<p class="loading">로딩 중...</p>';
 
-  CountryApi.loadCountries()
-    .then(function (countries) {
-      CountryUI.renderList(app, countries);
-    })
-    .catch(function (err) {
-      app.innerHTML = '<p class="error">국가 정보를 불러오는데 실패했습니다. ' + err.message + '</p>';
-    });
-})();
+//   CountryApi.loadCountries()
+//     .then(function (countries) {
+//       CountryUI.renderList(app, countries);
+//     })
+//     .catch(function (err) {
+//       app.innerHTML = '<p class="error">국가 정보를 불러오는데 실패했습니다. ' + err.message + '</p>';
+//     });
+// })();
